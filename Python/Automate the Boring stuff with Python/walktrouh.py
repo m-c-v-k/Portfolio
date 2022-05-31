8f3e88a0749ca49aca8f3023905dbc2985381c45
Marcus Klingborg <marcus.klingborg@live.jp>
2022-05-31 22:10:12 +0200

    New program, validate input, continued with string manipulation and string methods.

--- Python/Automate the Boring stuff with Python/walktrouh.py
+++ Python/Automate the Boring stuff with Python/walktrouh.py
@@ -381,3 +381,23 @@ display_inventory(inventory)
 
 
 ### Chapter 6 - Manipulating Strings ###
+
+'''
+while True:
+    print("Enter your age.")
+    age = input()
+
+    if age.isdecimal():
+        break
+
+    print("PLease enter a number for your age.")
+
+while True:
+    print("Select a new password (letters and numbers only):")
+    password = input()
+
+    if password.isalnum():
+        break
+
+    print("Passwords can only have letters and numbers.")
+'''
