7a341b1b6281c074198a2c38eda028d564d36dae
Marcus Klingborg <marcus.klingborg@live.jp>
2022-06-02 22:08:44 +0200

    Completed a minor program for priting out a dictionary

--- Python/Automate the Boring stuff with Python/walktrouh.py
+++ Python/Automate the Boring stuff with Python/walktrouh.py
@@ -401,3 +401,31 @@ while True:
 
     print("Passwords can only have letters and numbers.")
 '''
+
+'''
+def print_picnic(item_dictionary, left_width, right_width):
+    """ Prints a dictionary of picnic items so that it's easy to read.
+
+    Args:
+        item_dictionary (Dictionary): A dictionary containing items and ammount.
+        left_width (Int): An integer decidning on how far the print will be
+            adjusted
+        right_width (Int): An integer decidning on how far the print will be
+            adjusted
+    """
+
+    print('PICNIC ITEMS'.center(left_width + right_width, '-'))
+    for k, v in item_dictionary.items():
+        print(k.ljust(left_width, '.') + str(v).rjust(right_width))
+
+
+picnic_items = {
+    'sandwiches': 4,
+    'apples': 12,
+    'cups': 4,
+    'cookies': 8000
+}
+
+print_picnic(picnic_items, 12, 5)
+print_picnic(picnic_items, 20, 6)
+'''
