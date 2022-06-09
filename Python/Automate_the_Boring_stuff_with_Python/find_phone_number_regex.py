ef141ee6522d1a9e7834802683529b1348fafad8
Marcus Klingborg <marcus.klingborg@live.jp>
2022-06-09 21:47:31 +0200

    Edits to find phone number regex, areacode is now optional

--- Python/Automate_the_Boring_stuff_with_Python/find_phone_number_regex.py
+++ Python/Automate_the_Boring_stuff_with_Python/find_phone_number_regex.py
@@ -11,7 +11,7 @@ def search_phone_number_regex(text):
         text (string): any string to be checked for phone numbers
     '''
 
-    phone_number_regex = re.compile(r'\d{3}-\d{3}-\d{4}')
+    phone_number_regex = re.compile(r'(\d{3}-)?\d{3}-\d{4}')
     match_object = phone_number_regex.search(text)
     print("Phone numer found: " + match_object.group())
 
