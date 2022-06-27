d928ffd496bf056d10a476b58abbad91ea1f171e
Marcus Klingborg <marcus.klingborg@live.jp>
2022-06-27 22:01:55 +0200

    z-fix done in the rough, some minor fixes and optimizations to be done.

--- Python/My_Projects/z_fix/z_fix.py
+++ Python/My_Projects/z_fix/z_fix.py
@@ -9,10 +9,10 @@ import re
 
 
 def z_fix():
-    with open(str(os.getcwd()) + str(os.path.join('\\', 'Python', 'My_Projects', 'z_fix', 'fix.txt'))) as f:
+    with open(str(os.getcwd()) + str(os.path.join('\\', 'Python', 'My_Projects', 'z_fix', 'fix.txt')), 'r') as f:
         data = f.read()
 
-    eval_data = ast.literal_eval(data)
+        eval_data = ast.literal_eval(data)
 
     # Print all fix locations and their z-coordinate.
     for num in range(len(eval_data)):
@@ -24,22 +24,25 @@ def z_fix():
     while True:
         # Select existing fix location or new
         selected = str(
-            input('''Please enter your desired Z-location, 
-            create a new, continue, edit, remove, or quit.\n'''))
+            input('Please enter your desired Z-location, ' +
+                  'create a new, change z, edit, delete, or quit.\n'))
 
         if selected.lower() == 'new':
             new_fix(eval_data)
         elif selected.lower() == 'edit':
-            print("edit")
-        elif selected.lower() == 'remove':
-            print("remove")
+            edit_fix(eval_data)
+        elif selected.lower() == 'change z':
+            change_z()
+        elif selected.lower() == 'delete':
+            delete_fix(eval_data)
         elif selected.lower() == 'quit':
+            delete_file('measured.txt')
             print("Good bye.")
             break
         else:
             for i in places:
                 if selected.lower() == str(i).lower():
-                    calculate_z(eval_data[i])
+                    set_z(eval_data[i])
                     break
                 elif i == places[-1]:
                     print("That is not a valid choice, please try again.")
@@ -85,34 +88,51 @@ def new_fix(eval_data):
     return mo
 
 
-def calculate_z(z_position):
+def change_z():
+    '''change_z Changes the current Z-position and tells the user what
+    measure to put the laser to.
 
-    measured_z = float(
-        input("What is the current measurement for the fix position?\n"))
+    Returns:
+        Float: A float containing the new measured value.
+    '''
+
+    with open(str(os.getcwd()) + str(os.path.join('\\', 'Python', 'My_Projects', 'z_fix', 'measured.txt'))) as f:
+        data = f.read()
+
+        eval_data = ast.literal_eval(data)
+
+    z = list(eval_data.keys())
+    m = list(eval_data.values())
+    z_position = z[0]
+    old_measured = m[0]
+
     desired_z = float(input("Please enter the desired Z-position.\n"))
 
     z_position = float(z_position)
     change_by = z_position - desired_z
-    set_position = measured_z - abs(change_by) * 100
-    new_measured = measured_z + change_by * 100
+    new_measured = old_measured + change_by * 100
 
-    if z_position < desired_z:
-        print("You need to lower the laser measurer with " +
-              str(set_position) + ' centimeters to ' + str(new_measured) + '.')
-        # TODO save current z-position and measure.
-        save_current(new_measured, desired_z)
-    elif z_position > desired_z:
-        print("You need to raise the laser measurer with " +
-              str(set_position) + ' centimeters to ' + str(new_measured) + ' for a z-position of ' + desired_z + '.')
-        # TODO save current z-position and measure.
-        save_current(new_measured, desired_z)
-    else:
-        print("There's no need to change the laser measurement.")
-        # TODO save current z-position and measure.
-        save_current(new_measured, desired_z)
+    print("Set the laser to: " + str(new_measured) + '.')
+    save_current(desired_z, new_measured)
+
+    return new_measured
 
 
-def save_current(measured, z_pos):
+def set_z(z_position):
+    '''set_z let's the user save the measured z-fix measurement.
+
+    Args:
+        z_position (string): A string containing the measured z_fix position.
+    '''
+
+    measured_z = float(
+        input("What is the current measurement for the fix position?\n"))
+
+    print(str(z_position) + " set to " + str(measured_z) + ".")
+    save_current(z_position, measured_z)
+
+
+def save_current(z_pos, measured):
     '''save_current saves the current z-position and measurement, in order
     to allow the user to change from the current position to a new one.
 
@@ -124,22 +144,69 @@ def save_current(measured, z_pos):
         Dictionary: A dictionary over the latest measurement.
     '''
 
-    with open(str(os.getcwd()) + str(os.path.join('\\', 'Python', 'My_Projects', 'z_fix', 'continue.txt')), 'w+') as f:
+    with open(str(os.getcwd()) + str(os.path.join('\\', 'Python', 'My_Projects', 'z_fix', 'measured.txt')), 'w+') as f:
+        new_save = {}
         new_save[z_pos] = measured
         save_data = f.write(str(new_save))
 
     return new_save
 
 
-def continue_measure():
+def delete_file(file):
+    '''delete_file Deletes a specified file
 
-    with open(str(os.getcwd()) + str(os.path.join('\\', 'Python', 'My_Projects', 'z_fix', 'continue.txt'))) as f:
-        data = f.read()
+    Args:
+        file (String): The file name of the file to be deleted.
+    '''
 
-    eval_data = ast.literal_eval(data)
+    if os.path.exists(str(os.getcwd()) + str(os.path.join('\\', 'Python', 'My_Projects', 'z_fix', file))):
+        os.remove(str(os.getcwd()) + str(os.path.join('\\',
+                  'Python', 'My_Projects', 'z_fix', file)))
+    else:
+        print("The file does not exist.")
 
-    # Print current measure and then allow the user to change measurement to new z-position.
-    print(str(places[num]) + ': ' + str(fix_position))
+
+def delete_fix(eval_data):
+    '''delete_fix let's the user delete a saved fixed position.
+
+    Args:
+        eval_data (Dictionary): The dictionary containing all fixed positions.
+
+    Returns:
+        Dictionary: The dictionary containing all fixed positions.
+    '''
+
+    usr_inp = input("Please enter the fix you'd like to remove:\n")
+
+    eval_data.pop(usr_inp)
+
+    with open(str(os.getcwd()) + str(os.path.join('\\', 'Python', 'My_Projects', 'z_fix', 'fix.txt')), 'w+') as f:
+
+        f.write(str(eval_data))
+
+    return eval_data
+
+
+def edit_fix(eval_data):
+    '''edit_fix Let's the user edit a chosen fixed position.
+
+    Args:
+        eval_data (Dictionary): The dictionary containing all fixed positions.
+
+    Returns:
+        Dictionary: The dictionary containing all fixed positions.
+    '''
+
+    usr_inp_fix = input("Please enter the fix you'd like to edit:\n")
+    usr_inp_pos = input("Please enter the new z-pos for the fix:\n")
+
+    eval_data[usr_inp_fix] = usr_inp_pos
+
+    with open(str(os.getcwd()) + str(os.path.join('\\', 'Python', 'My_Projects', 'z_fix', 'fix.txt')), 'w+') as f:
+
+        f.write(str(eval_data))
+
+    return eval_data
 
 
 z_fix()
