9b12c8200014aea4c66234586e6384037c300bf2
Marcus Klingborg <marcus.klingborg@live.jp>
2022-10-18 16:27:30 +0200

    Started the Data Engineer pre-studies

---
+++ Data Engineer/Pre Studies/Week 1/Python/The Python Bible 1-5.py
@@ -0,0 +1,38 @@
+#! Python3
+
+# A walktrhough of the first five sections in 'The Python Bible' course
+
+### Section 1 - Course Introduction ###
+
+### Section 2 - Installing Python, Getting Started & A Top Secret Tip ###
+
+# 2.5
+'''
+print(1+1)
+print(105+10)
+'''
+
+### Section 3 - Simple Little Boxes - Variables in Python ###
+
+# 3.9
+'''
+number = 1
+print(number)
+print(type(number))
+
+number = "hello"
+print(number)
+print(type(number))
+'''
+# 3.10
+'''
+first_number = 1 + 1
+second_number = 105 + 10
+print(first_number)
+print(second_number)
+
+total = first_number + second_number
+print(total)
+'''
+
+### Section 4 - The 123s - Numbers in Python ###
