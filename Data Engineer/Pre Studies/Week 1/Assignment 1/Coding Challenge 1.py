541ea32dab2073dc2cba0f062f2a2e1bb1e130ca
Marcus Klingborg <marcus.klingborg@live.jp>
2022-10-20 21:35:40 +0200

    Started on Assignment 1

---
+++ Data Engineer/Pre Studies/Week 1/Assignment 1/Coding Challenge 1.py
@@ -0,0 +1,16 @@
+#! Python3
+
+"""Python Challenges:
+Coding Challenge 1 – Extending the Health Potion Project
+In this challenge we are going to expand on the project from the Udemy course section 4 about the 
+health potion. We are going to remake this project with a couple of more operations, but without 
+the difficulty system.
+
+
+You should create an empty python script and call it week1_1.py. Create a variable called health 
+with value 100. Then you should simulate getting attacked twice. Getting attacked means that you 
+subtract a random number between 20 and 30 from health. Remember to do this twice! After the 
+attacks you pick up a health potion, which here means that you add 10 plus a random number 
+between 10 and 15 to the health. So if you roll 14 as the random number for the potion (it should 
+be between 10 and 15 inclusive), you add 10+14=24 to the players health. In the end display 
+(print) the remaining health."""
