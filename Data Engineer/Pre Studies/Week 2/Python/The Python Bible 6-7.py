626b049af2e5bd4ad124e9685b7eddcb987adf1e
Marcus Klingborg <marcus.klingborg@live.jp>
2022-10-26 11:26:03 +0200

    7.44 Done

--- Data Engineer/Pre Studies/Week 2/Python/The Python Bible 6-7.py
+++ Data Engineer/Pre Studies/Week 2/Python/The Python Bible 6-7.py
@@ -171,3 +171,16 @@ print(b)
 print(students)
 '''
 # 7.44
+'''
+students = {
+    "Alice": {"id": "ID0001", "age": 26, "grade": "A"},
+    "Bob": {"id": "ID0002", "age": 27, "grade": "B"},
+    "Claire": {"id": "ID0003", "age": 17, "grade": "C"},
+    "Dan": {"id": "ID0004", "age": 21, "grade": "D"},
+    "Emma": {"id": "ID0005", "age": 22, "grade": "E"}
+}
+
+print(students["Dan"]["age"])
+print(students["Emma"]["id"], students["Emma"]["grade"])
+print(students.values())
+'''
