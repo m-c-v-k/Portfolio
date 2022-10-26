a760b57c80460cea66254c7f6fb0d0fe5c06cc33
Marcus Klingborg <marcus.klingborg@live.jp>
2022-10-26 13:58:31 +0200

    5.37, 5.38 Done

--- Data Engineer/Pre Studies/Week 2/SQL/SQL and PostgreSQL for Beginners.sql
+++ Data Engineer/Pre Studies/Week 2/SQL/SQL and PostgreSQL for Beginners.sql
@@ -45,3 +45,35 @@ WHERE release_date > '1999-12-31';
 
 SELECT * FROM movies
 WHERE movie_lang > 'English';
+
+-- 5.37
+SELECT first_name, last_name FROM actors
+WHERE first_name in ('Bruce', 'John');
+
+SELECT first_name, last_name FROM actors
+WHERE first_name in ('Bruce', 'John', 'Peter');
+
+SELECT first_name, last_name FROM actors
+WHERE first_name not in ('Bruce', 'John', 'Peter');
+
+SELECT actor_id, first_name, last_name FROM actors
+WHERE actor_id IN (2,3,4,5,6,8);
+
+SELECT actor_id, first_name, last_name FROM actors
+WHERE actor_id NOT IN (2,3,4,5,6,8);
+
+--5.38
+SELECT * FROM actors
+WHERE first_name LIKE 'P%';
+
+SELECT * FROM actors
+WHERE first_name LIKE 'Pe_';
+
+SELECT * FROM actors
+WHERE first_name LIKE '%r%';
+
+SELECT * FROM actors
+WHERE first_name LIKE '%rl%';
+
+SELECT * FROM actors
+WHERE first_name LIKE '__rl__';
