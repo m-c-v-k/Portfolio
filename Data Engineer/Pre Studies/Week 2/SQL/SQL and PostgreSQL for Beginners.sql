1ee331e39bfdd1bb745d2e8fa9240452e6ad07f5
Marcus Klingborg <marcus.klingborg@live.jp>
2022-10-26 14:30:54 +0200

    5.39 Done

--- Data Engineer/Pre Studies/Week 2/SQL/SQL and PostgreSQL for Beginners.sql
+++ Data Engineer/Pre Studies/Week 2/SQL/SQL and PostgreSQL for Beginners.sql
@@ -77,3 +77,13 @@ WHERE first_name LIKE '%rl%';
 
 SELECT * FROM actors
 WHERE first_name LIKE '__rl__';
+
+-- 5.39
+SELECT movie_name, release_date FROM movies
+WHERE release_date BETWEEN '1995-01-01' AND '1999-12-31';
+
+SELECT movie_name, movie_length FROM movies
+WHERE movie_length BETWEEN 90 AND 120;
+
+SELECT movie_name, movie_length FROM movies
+WHERE movie_lang BETWEEN 'E' AND 'P';
