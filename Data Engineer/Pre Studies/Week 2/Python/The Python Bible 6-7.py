3afe5693e98b87b66c693d3d1b1427d64e71e6ad
Marcus Klingborg <marcus.klingborg@live.jp>
2022-10-26 11:11:52 +0200

    7.43 Done

--- Data Engineer/Pre Studies/Week 2/Python/The Python Bible 6-7.py
+++ Data Engineer/Pre Studies/Week 2/Python/The Python Bible 6-7.py
@@ -152,3 +152,22 @@ print(a)
 print(b)
 print(c)
 '''
+# 7.43
+'''
+students = {}
+students = {"Alice": 25, "Bob": 27, "Claire": 17, "Dan": 21, "Emma": 22}
+print(students["Dan"])
+students["Fred"] = 25
+print(students["Fred"])
+students["Alice"] = 26
+print(students["Alice"])
+del students["Fred"]
+print(students.keys())
+a = list(students.keys())
+print(a)
+print(students.values())
+b = list(students.values())[2:]
+print(b)
+print(students)
+'''
+# 7.44
