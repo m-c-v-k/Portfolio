0521fed68def2a6438100184ad82208a47e380e1
Marcus Klingborg <marcus.klingborg@live.jp>
2022-10-31 15:12:57 +0100

    Project 7.2 Done

--- Data Engineer/Pre Studies/Week 3/Python/Projects/Project 7.2 - Pig Latin Translator.py
+++ Data Engineer/Pre Studies/Week 3/Python/Projects/Project 7.2 - Pig Latin Translator.py
@@ -4,11 +4,38 @@
 # A small program translating words to pig latin
 
 # Get sentence from user
+original = input("Please enter a sentence: ").strip().lower()
 
 # Split sentence into words
+words = original.split()
 
 # Loop through words and convert to pig latin
+pig_words = []
+
+for word in words:
+
+    # If starting with vowel, add "yay"
+    if word[0] in "aeiou":
+        new_word = word + "yay"
+        pig_words.append(new_word)
+
+    # Otherwise add first consonant cluster to end, and add "ay"
+    else:
+        vowel_pos = 0
+        for letter in word:
+            if letter not in "aeiou":
+                vowel_pos += 1
+            else:
+                break
+
+        cons = word[:vowel_pos]
+        the_rest = word[vowel_pos:]
+
+        new_word = the_rest + cons + "ay"
+        pig_words.append(new_word)
 
 # Stick words back together
+output = " ".join(pig_words)
 
 # Output the final string
+print(output)
