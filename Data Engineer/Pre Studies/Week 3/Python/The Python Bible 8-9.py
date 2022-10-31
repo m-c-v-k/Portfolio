6ebb28bc84456b0bc54cf51bf00d4916d33d9f14
Marcus Klingborg <marcus.klingborg@live.jp>
2022-10-31 14:53:16 +0100

    8.52 Done

--- Data Engineer/Pre Studies/Week 3/Python/The Python Bible 8-9.py
+++ Data Engineer/Pre Studies/Week 3/Python/The Python Bible 8-9.py
@@ -63,3 +63,14 @@ for key in students.keys():
         if "a" in name:
             print(name)
 '''
+# 8.52
+
+even_numbers = [x for x in range(1, 101) if x % 2 == 0]
+print(even_numbers)
+
+odd_numbers = [x for x in range(1, 101) if x % 2 == 1]
+print(odd_numbers)
+
+words = ["the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"]
+answer = [[w.upper(), w.lower(), len(w)] for w in words]
+print(answer)
