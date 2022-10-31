d70f44ec67d605f95d265f8b332e30674c2e677f
Marcus Klingborg <marcus.klingborg@live.jp>
2022-10-31 14:18:02 +0100

    Week 3 Python 8.48 Done

---
+++ Data Engineer/Pre Studies/Week 3/Python/The Python Bible 8-9.py
@@ -0,0 +1,32 @@
+#! Python3
+
+# A walkthrough of section 8 and 9 in 'The Python Bible' course
+
+# 8.48
+'''
+while True:
+    print("Do this")
+
+while 2 > 1:
+    print("Hello")
+
+while False:
+    print("Hello")
+
+number = 1
+
+while number <= 10:
+
+    if number % 2 == 0:
+        print(number)
+    number += 1
+
+l = []
+
+while len(l) < 3:
+    new_name = input("Please inser a name: ").strip().capitalize()
+    l.append(new_name)
+
+print("Sprry list is full")
+print(l)
+'''
