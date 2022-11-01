f164a58689b3ae58f3754fda04310e3c6afc61a8
Marcus Klingborg <marcus.klingborg@live.jp>
2022-11-01 09:55:13 +0100

    6.58 Done

--- Data Engineer/Pre Studies/Week 3/SQL/SQL and PostgreSQL for Beginners.sql
+++ Data Engineer/Pre Studies/Week 3/SQL/SQL and PostgreSQL for Beginners.sql
@@ -25,3 +25,16 @@ WHERE domestic_takings > 100.0;
 
 SELECT SUM(movie_length) from movies
 WHERE movie_lang = 'Chinese';
+
+-- 6.58
+SELECT MAX(movie_length) FROM movies;
+SELECT MIN(movie_length) FROM movies;
+
+SELECT MIN(movie_length) FROM movies
+WHERE movie_lang = 'Japanese';
+
+SELECT MAX(release_date) FROM movies;
+SELECT MIN(release_date) FROM movies;
+
+SELECT MAX(movie_name) FROM movies;
+SELECT MIN(movie_name) FROM movies;
