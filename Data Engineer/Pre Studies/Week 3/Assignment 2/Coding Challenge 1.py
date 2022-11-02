af2583defbae637be8aee97e65633efb73d64f06
Marcus Klingborg <marcus.klingborg@live.jp>
2022-11-02 09:24:15 +0100

    Python Coding Challenge 1 Done

---
+++ Data Engineer/Pre Studies/Week 3/Assignment 2/Coding Challenge 1.py
@@ -0,0 +1,10 @@
+#! Python3
+
+# Coding Challenge 1 – For-loops
+
+# Write a program that outputs the numbers from 50 to 0 in the console. Use the range() function
+# together with a for-loop.
+
+# Loop
+for x in range(50, -1, -1):
+    print(x)
