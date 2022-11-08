7d538e1d5454bc85fa7d6426beee6f0c54fb8f94
Marcus Klingborg <marcus.klingborg@live.jp>
2022-11-08 11:34:28 +0100

    Challenge 8.1 Done

---
+++ Data Engineer/Pre Studies/Week 4/SQL/Challenges/Challenges 8.1 - Joins.sql
@@ -0,0 +1,21 @@
+-- Section 7 - Challenge 1 - Database Relationships
+
+-- 1
+SELECT d.first_name, d.last_name, mo.movie_name, mo.release_date
+FROM directors d
+JOIN movies mo USING (director_id)
+WHERE mo.movie_lang IN ('Chinese', 'Korean', 'Japanese');
+
+-- 2
+SELECT mo.movie_name, mo.release_date, mr.international_takings
+FROM movies mo
+JOIN movie_revenues mr USING (movie_id)
+WHERE mo.movie_lang = 'English';
+
+-- 3
+SELECT mo.movie_name, mr.domestic_takings, mr.international_takings
+FROM movies mo
+JOIN movie_revenues mr USING (movie_id)
+WHERE mr.domestic_takings IS NULL
+OR mr.international_takings IS NULL
+ORDER BY mo.movie_name;
