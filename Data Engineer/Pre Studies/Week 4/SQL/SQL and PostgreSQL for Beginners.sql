65e18454c1e0a89fa2c70fda2e7bc2a3882b236a
Marcus Klingborg <marcus.klingborg@live.jp>
2022-11-08 10:53:05 +0100

    SQL 8.78 Done

--- Data Engineer/Pre Studies/Week 4/SQL/SQL and PostgreSQL for Beginners.sql
+++ Data Engineer/Pre Studies/Week 4/SQL/SQL and PostgreSQL for Beginners.sql
@@ -6,4 +6,22 @@
 
 -- Section 8: SQL & PostgreSQL: Joinning Tables
 
---
+-- 8.78
+SELECT * FROM directors;
+
+INSERT INTO directors (first_name, last_name, date_of_birth, nationality)
+VALUES ('Christopher', 'Nolan', '1970-07-30', 'Brittish');
+
+SELECT * FROM movies;
+
+SELECT directors.director_id, directors.first_name, directors.last_name, movies.movie_name
+FROM directors
+INNER JOIN movies ON directors.director_id = movies.director_id;
+
+SELECT directors.director_id, directors.first_name, directors.last_name, movies.movie_name
+FROM directors
+INNER JOIN movies ON directors.director_id = movies.director_id
+WHERE movies.movie_lang = 'Japanese'
+ORDER BY movies.movie_length;
+
+-- 8.79
