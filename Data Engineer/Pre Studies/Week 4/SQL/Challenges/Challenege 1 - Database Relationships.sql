874947835926b379a254dfa28eac4f39cc6ebb1b
Marcus Klingborg <marcus.klingborg@live.jp>
2022-11-08 09:47:12 +0100

    SQL Section 7  and Challenge Done

---
+++ Data Engineer/Pre Studies/Week 4/SQL/Challenges/Challenege 1 - Database Relationships.sql
@@ -0,0 +1,8 @@
+-- Section 7 - Challenge 1 - Database Relationships
+
+-- 1: one to many relationship
+
+-- 2: one to one relationship
+
+-- 3: Users and Playlists: on to many relationship
+--    Playlists and Songs: many to many relationship
---
+++ Data Engineer/Pre Studies/Week 4/SQL/SQL and PostgreSQL for Beginners.sql
@@ -0,0 +1,9 @@
+-- A walkthrough of section six in 'SQL & PostgreSQL for Beginners: Become an SQL Expert' course
+
+-- Section 7: Database Relationships --
+
+-- No code examples.
+
+-- Section 8: SQL & PostgreSQL: Joinning Tables
+
+--
