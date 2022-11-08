4560df24bcdf1fd9143f2e71656a8f0d1a0bb2f8
Marcus Klingborg <marcus.klingborg@live.jp>
2022-11-08 11:01:29 +0100

    8.79 Done

--- Data Engineer/Pre Studies/Week 4/SQL/SQL and PostgreSQL for Beginners.sql
+++ Data Engineer/Pre Studies/Week 4/SQL/SQL and PostgreSQL for Beginners.sql
@@ -25,3 +25,15 @@ WHERE movies.movie_lang = 'Japanese'
 ORDER BY movies.movie_length;
 
 -- 8.79
+SELECT d.director_id, d.first_name, d.last_name, d.last_name, mo.movie_name
+FROM directors d
+JOIN movies mo ON d.director_id = mo.director_id
+WHERE mo.movie_lang = 'Japanese'
+ORDER BY mo.movie_length;
+
+SELECT mo.movie_name, mr.domestic_takings, mr.international_takings
+FROM movies mo
+JOIN movie_revenues mr ON mo.movie_id = mr.movie_id
+ORDER BY mr.domestic_takings;
+
+-- 8.80
