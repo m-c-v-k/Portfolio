c98304c3a380a73b85a1bd6f75efb961167bc11d
Marcus Klingborg <marcus.klingborg@live.jp>
2022-11-09 15:30:16 +0100

    Week4.2, 4.3a Done

--- excercise week4.session.sql
+++ excercise week4.session.sql
@@ -13,5 +13,22 @@ ORDER BY country, city;
 -- paper subscriptions. The output should have three columns: name, city and country. Hint:
 -- Use JOIN and WHERE.
 
-SELECT name, city, country from customer
-JOIN
+SELECT c.name, c.city, c.country from customer c
+JOIN paper_subscription ps ON c.id = ps.customer_id
+WHERE status = 'Inactive'
+ORDER BY c.country, c.city, c.name;
+
+-- 3a) Which articles do customers find the most interesting? Write a query to find which
+-- customers have read what articles how many times. The output should have three
+-- columns: name, article name and number of times read.
+
+SELECT c.name, ar.article_id, COUNT(ar.article_id) AS read_times
+FROM customer c
+JOIN article_reads ar ON c.id = ar.customer_id
+GROUP BY c.name, ar.article_id
+ORDER BY c.name, ar.article_id;
+
+-- 3b) Based on the above query, write a query find which 10 customers have read the most
+-- distinct articles. The output should have two columns: name and number of distinct articles
+-- read. The results should be sorted in descending order. Hint: Use ORDER BY and LIMIT, in
+-- addition to the commands above. 
