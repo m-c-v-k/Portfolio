7cf6957253bacbaf21219052226511bf25b66a11
Marcus Klingborg <marcus.klingborg@live.jp>
2022-11-09 10:07:00 +0100

    9.106 Done

--- Data Engineer/Pre Studies/Week 4/SQL/SQL and PostgreSQL for Beginners.sql
+++ Data Engineer/Pre Studies/Week 4/SQL/SQL and PostgreSQL for Beginners.sql
@@ -180,3 +180,17 @@ WHERE mo.movie_id IN (
     WHERE international_takings > domestic_takings
 );
 
+-- 9.106
+SELECT d1.first_name, d1.last_name, d1.date_of_birth
+FROM directors d1
+WHERE d1.date_of_birth = (
+    SELECT MIN(date_of_birth) FROM directors d2
+    WHERE d2.nationality = d1.nationality
+);
+
+SELECT mo1.movie_name, mo1.movie_lang, mo1.movie_length
+FROM movies mo1
+WHERE mo1.movie_length = (
+    SELECT MAX(movie_length) FROM movies mo2
+    WHERE mo1.movie_lang = mo2.movie_lang
+);
