e5a3a451791928fda378fa6d121c4ad39cfc9384
Marcus Klingborg <marcus.klingborg@live.jp>
2022-11-22 10:09:50 +0100

    adding requirements

--- flask/phonelist/venv/Scripts/activate.bat
+++ flask/phonelist/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\MarcusKlingborg\Data Engineer\portfolio\flask\phonelist\venv
+set VIRTUAL_ENV=C:\Users\MarcusKlingborg\Data Engineer\Portfolio\flask\phonelist\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
