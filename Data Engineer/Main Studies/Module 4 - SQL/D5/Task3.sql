debf5b67a7851bd65d601d6e96c734e993b2f9f0
Marcus Klingborg <marcus.klingborg@live.jp>
2022-12-05 10:02:05 +0100

    Task 1, Task 2 Done Task3 started

---
+++ Data Engineer/Main Studies/Module 4 - SQL/D5/Task3.sql
@@ -0,0 +1,20 @@
+-- At PostgreSQL Tutorial you try out more on LIMIT and OFFSET, still using DVDRental
+-- database.
+
+-- Task 3 – CTE
+
+-- 1. Use CTE to create a table that classifies customer_ids according to the total
+
+-- amount having spent in payments. More than 120 are 'Big doggo's, less than 80 are
+
+-- 'Smol pupper's and in between are regular 'Doggo's. Then select and list all the Big
+
+-- Doggos.
+
+-- 2. Create a new table 'deleted_payments', with one column, INT primary key
+
+-- 'payment_id'.
+
+-- 3. Use WITH to create a Data-Modifying Statement deleting payments made on
+
+-- valentines day 2007 or before, and moving them into the table deleted_payments
