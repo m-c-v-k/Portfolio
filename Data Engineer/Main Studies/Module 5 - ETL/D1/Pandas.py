7d9e1c6a33ce5125eb3a463ffb07c6bec315d5db
Marcus Klingborg <marcus.klingborg@live.jp>
2022-12-07 15:30:45 +0100

    Somehow it worked...

--- Data Engineer/Main Studies/Module 5 - ETL/D1/Pandas.py
+++ Data Engineer/Main Studies/Module 5 - ETL/D1/Pandas.py
@@ -6,22 +6,37 @@ import os
 
 CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 automobile_path = CURR_DIR_PATH + "/Automobile_data.csv"
+automobile_cleaned_path = CURR_DIR_PATH + "/Automobile_data_cleaned.csv"
+
 
 automobile_df = pd.read_csv(automobile_path)
-
 print(automobile_df.to_string())
 
-print(automobile_df.head(5))
-print(automobile_df.tail(5))
+# print(automobile_df.head(5))
+# print(automobile_df.tail(5))
 
 automobile_df = pd.read_csv(
     automobile_path,
     na_values={
-        'price': ["?", "n.a"]
+        'price': ["?", "n.a"],
+        'num-of-cylinders': ["?", "n.a"],
+        'engine-type': ["?", "n.a"],
+        'horsepower': ["?", "n.a"],
+        'average-mileage': ["?", "n.a"]
     }
 )
-
 print(automobile_df.to_string())
-automobile_df.to_csv(automobile_path)
+automobile_df.to_csv(automobile_cleaned_path)
 
-print(automobile_df.to_string())
+"""
+
+df = pd.read_csv(automobile_cleaned_path)
+
+df = df[['company', 'price']][df.price == df['price'].max()]
+print(df)
+
+
+manufracturer = df.groupby('company')
+toyata_df = manufracturer.get_group('toyota')
+print(toyata_df)
+"""
