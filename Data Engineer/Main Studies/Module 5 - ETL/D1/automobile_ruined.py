5fb580f7ed3dc3a6748489881508e60cfd01ab68
Marcus Klingborg <marcus.klingborg@live.jp>
2022-12-07 20:59:14 +0100

    automobile ruined half done

--- Data Engineer/Main Studies/Module 5 - ETL/D1/automobile_ruined.py
+++ Data Engineer/Main Studies/Module 5 - ETL/D1/automobile_ruined.py
@@ -10,5 +10,23 @@ ruined_automobile_path = CURR_DIR_PATH + "/ruined_automobile_data-1.csv"
 cleaned_automobile_path = CURR_DIR_PATH + "/cleaned_automibile.csv"
 
 df = pd.read_csv(ruined_automobile_path)
-
 print(df.isnull().any())
+
+df = pd.read_csv(
+    ruined_automobile_path,
+    na_values={
+        'company': ['NaN', '?', 'n.a'],
+        'body-style': ['NaN', '?', 'n.a'],
+        'wheel-base': ['NaN', '?', 'n.a'],
+        'num-of-cylinders': ['NaN', '?', 'n.a'],
+        'price': ['NaN', '?', 'n.a']
+    }
+)
+
+df['company'] = df['company'].fillna(-1)
+df['body-style'] = df['body-style'].fillna(-1)
+df['wheel-base'] = df['wheel-base'].fillna(-1)
+df['num-of-cylinders'] = df['num-of-cylinders'].fillna(-1)
+df['price'] = df['price'].fillna(-1)
+
+print(df.to_string())
