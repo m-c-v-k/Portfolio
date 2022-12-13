cd45f21b103e6f863778b3f2b558c4844b88faea
Marcus Klingborg <marcus.klingborg@live.jp>
2022-12-13 21:03:42 +0100

    added data files

--- Data Engineer/Main Studies/Module 5 - ETL/D5/private learnings/pandas_exercise.py
+++ Data Engineer/Main Studies/Module 5 - ETL/D5/private learnings/pandas_exercise.py
@@ -1,5 +1,24 @@
 import pandas as pd
+import os
+
+CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 df = pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
-
 print(df)
+
+
+df = pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 'Sue':
+                  ['Pretty good.', 'Bland.']})
+print(df)
+
+s = pd.Series([1, 2, 3, 4, 5])
+print(s)
+
+s = pd.Series([30, 35, 40.], index=['2015 Sales',
+              '2016 Sales', '2017 Sales'], name='Product A')
+print(s)
+
+wine_reviews = pd.read_csv(f"{CURR_DIR_PATH}/data/winemag-data-130k-v2.csv")
+
+print(wine_reviews.shape)
+print(wine_reviews.head())
