7c08af05270d334f57a457377c7bfa2b0840b879
Marcus Klingborg <marcus.klingborg@live.jp>
2022-12-29 14:12:14 +0100

    Minor changes to exercise 9

--- Data Engineer/Main Studies/Module 7 - Visualization/D2/exercise_09.py
+++ Data Engineer/Main Studies/Module 7 - Visualization/D2/exercise_09.py
@@ -6,11 +6,6 @@ import os
 
 # 9
 
-CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-data_path = f'{CURR_DIR_PATH}/../D1/data/forsaljning.csv'
-
-df = pd.read_csv(data_path)
-
 figure = make_subplots(rows=1, cols=2)
 
 country = {
--- Data Engineer/Main Studies/Module 7 - Visualization/D2/exercise_10.py
+++ Data Engineer/Main Studies/Module 7 - Visualization/D2/exercise_10.py
@@ -5,8 +5,3 @@ import pandas as pd
 import os
 
 # 10
-
-CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-data_path = f'{CURR_DIR_PATH}/../D1/data/forsaljning.csv'
-
-df = pd.read_csv(data_path)
